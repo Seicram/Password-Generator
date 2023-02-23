cf7808024d22d7b75bb0567406ab32daa27e10af
Marcies Smith <marcies.smith@gmail.com>
2023-02-23 13:57:10 -0500

    added more if/else code

--- Develop/script.js
+++ Develop/script.js
@@ -18,11 +18,45 @@ if (numberCharacters < 6 || numberCharacters > 134) {
   return "Select the correct number of characters";
 }
 else if (isNaN(numberCharacters)) {
-  numberCharacters = prompt("Please enter a valid number.");
+  numberCharacters = prompt("Enter a valid number.");
 }
 else {
   message("Password is " + numberCharacters + " characters");
   
 }
+  isLowercase = confirm("You can select lowercase characters");
+  if (isLowercase) {
+    var makeLowercase = alert("The password will contain lowercase characters");
+  }
+  else {
+    alert("The password will not have lowercase characters");
+  }
+
+  isUppercase = confirm("Uppercase characters are available");
+  if (isUppercase) {
+    alert("The password will contain uppercase characters");
+  }
+  else {
+    alert("The password will not have uppercase characters");
+  }
+
+  isNumbers = confirm("Numerical values are available");
+  if (isNumbers) {
+    alert("The password will contain numberical values");
+  }
+  else {
+    alert("The password will not contain numerical values");
+  }
+
+  isSpecial = confirm("Special characters are available");
+  if (isSpecial) {
+    alert("The password will contain special characters");
+  }
+  else {
+    alert("The password will not contain special characters.");
+  }
+
+  
   // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
