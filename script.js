323b694058fd2519d2e1c4813c9162d7d6a1d1e0
Marcies Smith <marcies.smith@gmail.com>
2023-04-18 11:40:47 -0400

    fixed js code, was not deploying

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var special = ['!', '@', '#', '$', '%', "^", '&', '*', '(', ')', '-', '_', '=', '+', ']', '[', '}', '{', '|', ',', '>', '<', '/', '?', '~', '.', '`'];
-var possible = [];
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -81,12 +83,38 @@ else {
 };
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  let passwordLength = prompt("Enter password length (minimum 8 characters):");
+   if (passwordLength < 8) {
+    alert("Password length must be at least 8 characters");
+    return;
+  }
+  else if (passwordLength > 128) {
+    alert("Password length must be at most 128 characters");
+    return;
+  } 
   
-  // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  let includeLowercase = confirm("Should I include lowercase characters?");
+  let includeUppercase = confirm("Should I include uppercase characters?");
+  let includeNumbers = confirm("Should I include numerical characters?");
+  let includeSpecial = confirm("Should I include special characters?");
 
-function writePassword() {
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  let charTypes = [];
+  if (includeLowercase) charTypes.push("abcdefghijklmnopqrstuvwxyz");
+  if (includeUppercase) charTypes.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  if (includeNumbers) charTypes.push("1234567890");
+  if (includeSpecial) charTypes.push(" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+  if (charTypes.length === 0) {
+    alert("At least one character type must be selected");
+    return;
+  }
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let charType = charTypes[Math.floor(Math.random() * charTypes.length)];
+    password = password + charType.charAt(Math.floor(Math.random() * charType.length));
+  } 
+  return password;
 }
+  // Add event listener to generate button
+  
+  generateBtn.addEventListener("click", writePassword);
